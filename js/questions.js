7b246c97e2215eae392f54e07efc7220cfdf64ec
Mrstrange9966 <154722333+Mrstrange9966@users.noreply.github.com>
2023-12-24 13:42:33 +0530

    Added files

---
+++ js/questions.js
@@ -0,0 +1,181 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "what is a BPO??",
+    answer: "Business Process Outsourcing",
+    options: [
+      "Business Process Outsourcing",
+      "Business Process Outsource",
+      "Business Project Outsourcing",
+      "Business Product Outsourcing"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What are the factors which have an impact when deciding on a BPO destination?",
+    answer: "All of the above",
+    options: [
+      "Reliable telecommunications infrastructure",
+      "Cost of labour",
+      "Strong regulatory framework",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The most common data conversion options are",
+    answer: "Converting raw data into Microsoft Office",
+    options: [
+      "Converting raw data into Microsoft Office",
+      "Convert from PDF to Word",
+      "Convert an audio file into text",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 4,
+    question: "The main characteristics of Knowledge Process Outsourcing are?",
+    answer: "All of the above",
+    options: [
+      "Process are not easily codified",
+      "The agent has to interpret the information",
+      "The agent is expected to exercise judgement",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What do you think is the most important justification for a company to consider offshore decision??",
+    answer: "All of the above",
+    options: [
+      "Overall cost reduction",
+      "Productivity improvement",
+      "Efficiency/Effectiveness",
+      "All of the above"
+    ]
+  },
+  
+ {
+    numb: 6,
+    question: "A TRUE statement between Low End and High End BPO is?",
+    answer: "High End BPO have more ruled based processes than Low End BPO and therefore require more skilled manpower",
+    options: [
+      "Both Low End and High End BPO are mainly Call Centers",
+      "Low End BPOs offer relatively much higher salary than High End BPO",
+      "High End BPO have more ruled based processes than Low End BPO and therefore require more skilled manpower",
+      "People working in Low End BPO companies usually stay much longer in that company than those working in High End BPO"
+    ]
+  },
+  
+  {
+    numb: 7,
+    question: "BPO companies involved in transcription normally?",
+    answer: "Transforming an audio file into text",
+    options: [
+      "Capture data and translate it into text",
+      "Listen to a conversation on-line and translate it into text",
+      "Transforming an audio file into text",
+      "Translate from one language into a different language"
+    ]
+  },
+  
+  {
+    numb: 8,
+    question: "A BPO company specializing in image processing may provide the following services?",
+    answer: "All of the above",
+    options: [
+      "Image Scanning",
+      "Image Conversion",
+      "Archiving",
+      "All of the above"
+    ]
+  },
+  
+  {
+    numb: 9,
+    question: "In order to improve its global attractiveness index in BPO coming years, India should leverage on its:?",
+    answer: "Improving national competencies into global talents",
+    options: [
+      "Telecom Infrastructure",
+      "Skills and competencies of its BPO Workforce",
+      "Improving national competencies into global talents",
+      "All of the above"
+    ]
+  },
+  
+  {
+    numb: 10,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  
+  {
+    numb: 11,
+    question: "In terms of cost characteristics, which of the following are outsourcing location decisions from low cost to high cost?",
+    answer: "All of the above",
+    options: [
+      "Overall cost reduction",
+      "Productivity improvement",
+      "Efficiency/Effectiveness",
+      "All of the above"
+    ]
+  },
+  
+  {
+    numb: 12,
+    question: "Most commonly used databases for making data entry is?",
+    answer: "All of the Above",
+    options: [
+      "Microsoft SQL Server, ASP, Ms Access, Oracle",
+      "My SQL, FoxPro, ASP, Ms Access",
+      "Oracle, My SQL, FoxPro, Visual Basic",
+      "All of the Above"
+    ]
+  },
+  
+  {
+    numb: 13,
+    question: "what is the THREE PDF formats commonly used in Data Capture/Data Entry Operation?",
+    answer: "PDF Formatted Text, PDF searchable image, PDF Image Only",
+    options: [
+      "PDF Formatted Text, PDF searchable image, PDF Image Only",
+      "PDF Acrobat Reader, PDF Acrobat Writer, PDF Scanner",
+      "PDF to Word Conversion, Word to PDF Conversion, PDF to Image Conversion",
+      "All of the above"
+    ]
+  },
+  
+  {
+    numb: 14,
+    question: "who orders BPOs?",
+    answer: "All of the above",
+    options: [
+      "Banks.",
+      "Wall Street Firms.",
+      "Private Mortgage Insurance Companies",
+      "All of the above"
+    ]
+  },
+  
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
